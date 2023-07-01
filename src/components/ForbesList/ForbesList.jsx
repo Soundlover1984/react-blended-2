d05697d68801440b4d8962f18493dcc70f0d973a
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-07-01 12:54:28 +0300

    1

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -8,6 +8,30 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+import PropTypes from 'prop-types';
+
+export const ForbesList = ({ list }) => {
+  
+  return <LeaderBoard>
+  <BoardHeader>
+    <BoardTitle>
+      <TitleTop>Forbes</TitleTop>
+      <TitleBottom>Leader board</TitleBottom>
+    </BoardTitle>
+  </BoardHeader>
+
+  <LeaderBoardProfiles>
+      {list.map(item => {
+        return <ForbesListItem key={item.id} avatar={item.avatar}
+          name={item.name}
+          capital={item.capital}
+          isIncrease={ item.isIncrease} />
+   })}
+  </LeaderBoardProfiles>
+  </LeaderBoard>;
+  
 };
+
+ForbesList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.string.isRequired}).isRequired).isRequired,
+}
