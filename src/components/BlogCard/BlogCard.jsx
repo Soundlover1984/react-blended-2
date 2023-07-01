311edd0c272bb9d3ea9d8e24f9287a2c66fb9ba3
Denys Kovtun <denys.kovtun@gmail.com>
2023-07-01 11:11:17 +0300

    task

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -14,6 +14,28 @@ import {
   Date,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import { getFormattingDistanceToNow } from 'helpers/formatDate';
+
+export const BlogCard = ({poster, tag, title, description, userName, avatar, postedAt}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={userName} />
+          <UserInfo>
+            <UserName>{userName}</UserName>
+            <Date>{getFormattingDistanceToNow(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
